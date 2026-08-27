1d7b3bc820aa2c98796811b87526cf72670dd711
shanshurbi <134719471+shanshurbi@users.noreply.github.com>
2026-08-27 15:50:29 -0400

    Update dim_client.sql
    cambio de join de sap_id a rut_company = tax_id

--- models/silver/dim_client.sql
+++ models/silver/dim_client.sql
@@ -26,23 +26,26 @@ mc_raw AS (
 ),
 aatn_clients AS (
     SELECT
-        bronze_id, tenant_id, ingested_at,
-        client_id                                   AS source_id,
-        email                                       AS email_raw,
-        LOWER(email)                                AS email,
-        LOWER(email)                                AS email_clean,
-        contact_name                                AS first_name,
-        CAST(NULL AS STRING)                        AS last_name,
-        CAST(NULL AS STRING)                        AS group_id,
-        customer_type                               AS group_name,
-        tax_id                                      AS rut,
-        CAST(NULL AS STRING)                        AS company_id,
-        CAST(NULL AS STRING)                        AS company_role_id,
-        CAST(NULL AS STRING)                        AS company_job_title,
-        FALSE                                       AS is_technical_account,
-        CAST(created_at AS DATETIME)                AS created_at,
-        CAST(NULL AS DATETIME)                      AS updated_at
-    FROM {{ ref('stg_aatn__clients') }}
+        c.bronze_id, c.tenant_id, c.ingested_at,
+        client_id                                    AS source_id,
+        c.email                                      AS email_raw,
+        LOWER(c.email)                               AS email,
+        LOWER(c.email)                               AS email_clean,
+        c.contact_name                               AS first_name,
+        CAST(NULL AS STRING)                         AS last_name,
+        CAST(NULL AS STRING)                         AS group_id,
+        c.customer_type                              AS group_name,
+        c.tax_id                                     AS rut,
+        co.source_id                                 AS company_id,
+        CAST(NULL AS STRING)                         AS company_role_id,
+        CAST(NULL AS STRING)                         AS company_job_title,
+        FALSE                                        AS is_technical_account,
+        CAST(c.created_at AS DATETIME)               AS created_at,
+        CAST(NULL AS DATETIME)                       AS updated_at
+    FROM {{ ref('stg_aatn__clients') }} c
+    LEFT JOIN {{ ref('stg_aatn__companies') }} co
+        ON c.tax_id = co.rut_company
+        AND c.tenant_id = co.tenant_id
 ),
 aatn_pending AS (
     SELECT
